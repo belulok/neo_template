5eb74af5823ef5e39ef0503674ba40e22c6cbfc8
belulok_sebastian <belulok_sebastian@hotmail.com>
2022-05-14 13:17:52 +0800

    initial commit

---
+++ src/bot.js
@@ -0,0 +1,10 @@
+import { config } from 'dotenv';
+config({ path: '.env' });
+
+import { Client, Intents } from 'discord.js';
+
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+bot.on('ready', () => console.log('ready'));
+
+bot.login(process.env.BOT_TOKEN);
